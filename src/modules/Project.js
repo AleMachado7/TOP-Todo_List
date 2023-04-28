5ae5768b6e910de0e263cafb4ef2e2f587c1ba9c
Alexandre <aleaxe7@gmail.com>
2023-04-27 21:45:50 -0300

    Modidfy project class adding new method

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -3,6 +3,20 @@ class Project {
     this._name = name
     this._taskList = new Array()
   }
+
+  addTask(Task) {
+    this._taskList.push(Task)
+  }
+
+  removeTask(Task) {
+    if (this._taskList.indexOf(Task) < 0) return
+
+    this._taskList.splice(this._taskList.indexOf(Task), 1)
+  }
+
+  getName() {
+    return this._name
+  }
 }
 
 export default Project
