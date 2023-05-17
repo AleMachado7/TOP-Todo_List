26c603214310fb2f5e7bbda2fca00714d36b7f70
Alexandre <aleaxe7@gmail.com>
2023-05-16 21:56:35 -0300

    add new lib that will later be used to handle dates of the tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./style.css"
+import UserInterface from "./modules/UserInterface"
+import Project from "./modules/Project"
+import Storage from "./modules/Storage"
+
+UserInterface.loadStorageProjects()
+
+const newProjectButton = document.getElementById("new-project-button")
+newProjectButton.addEventListener("click", () => {
+  let newProjectName = prompt("What's the name of the new project?")
+  newProjectName = newProjectName
+    ?.replaceAll("<", "")
+    .replaceAll(">", "")
+    .trim()
+
+  if (newProjectName?.length > 0) {
+    const project = new Project(newProjectName)
+    UserInterface.loadProject(project)
+    Storage.addNewProject(project)
+  }
+})
